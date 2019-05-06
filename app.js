c524142cb3914a6163dd0ad9140d642834d2b3f6
Hajredin Abidu <diniabdiu@gmail.com>
2019-05-06 23:43:10 +0200

    login auth

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express                 = require('express'),
     app                     = express(),
-    cons                    = require('consolidate'),
     path                    = require('path'),
     mongoose                = require('mongoose'),
     passport                = require('passport'),
@@ -12,8 +11,15 @@ var express                 = require('express'),
 mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true });
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true}));
+
 app.use(require('express-session')({
     secret: 'Rusty is the best',
     resave: false,
@@ -23,35 +29,59 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-app.use(express.static(__dirname + '/public'));
-app.engine('html', cons.swig)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-
+// Routes
 app.get('/', function(req, res) {
-    res.render('signin');
+    res.render('home');
 });
-
-app.get('/index', function(req, res) {
+app.get('/index',isLoggedIn, function(req, res) {
     res.render('index');
 });
-// Handling user sign up-signin=register
-app.post('/signin', function(req, res) {
+app.get('/signup', function(req, res) {
+    res.render('signup');
+});
+
+// Handling user sign up-signup=register
+app.post('signup', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
     User.register(new User({username}), password, function(err, user){
         if(err) {
             console.log(err);
-            return res.render('signin');
+            return res.render('signup');
         }
         passport.authenticate('local')(req, res, function() {
             res.redirect('index');
         });
     });
 });
+
+// Login routers
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+// Render login form
+// Login logic
+app.post('/login',passport.authenticate('local', {
+    successRedirect: '/index',
+    failureRedirect: '/login'
+}) ,function(req, res) {
+    
+});
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 // app.get('/register', function(req, res) {
 //     res.render('register');
